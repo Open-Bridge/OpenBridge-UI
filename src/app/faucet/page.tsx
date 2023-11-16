d69aed48167a4b426db03870026b93592c5fc39f
Hexdee <lukmansaheed79@gmail.com>
2023-11-16 07:39:18 +0100

    Add chain selection to faucet

--- src/app/faucet/page.tsx
+++ src/app/faucet/page.tsx
@@ -11,9 +11,10 @@ import React from 'react'
 export default function Home() {
     const [token, setToken] = React.useState("");
     const [user, setUser] = React.useState("");
+    const [chain, setChain] = React.useState("");
 
     const claimToken = () => {
-        console.log({user, token});
+        console.log({user, token, chain});
     }
 
   return (
@@ -22,12 +23,20 @@ export default function Home() {
         <div className="w-[88%] h-[80vh] flex justify-center items-center pt-5 mt-0 mb-20 ml-auto mr-auto pl-[1.06rem] sm:pl-8 overflow-x-hidden">
             <div className=" text-white h-auto flex flex-wrap justify-center items-center mt-[200px] text-center xl:text-left pr-[1.06rem] sm:pr-8 ">
             <p className='mb-8 text-3xl'>Token Faucet</p>
-            <select onChange={(e) => setToken(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option selected>Select token</option>
-                <option value="contract_address">Token 1</option>
-                <option value="contract_address">Token 2</option>
-                <option value="contract_address">Token 3</option>
-            </select>
+            <div className='w-full flex justify-between'>
+                <select onChange={(e) => setToken(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[49%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected>Select token</option>
+                    <option value="contract_address">Token 1</option>
+                    <option value="contract_address">Token 2</option>
+                    <option value="contract_address">Token 3</option>
+                </select>
+                <select onChange={(e) => setChain(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[49%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option selected>Select chain</option>
+                    <option value="chain_id">Polygon</option>
+                    <option value="chain_id">Ethereum</option>
+                    <option value="chain_id">BSC</option>
+                </select>
+            </div>
             <input onChange={(e) => setUser(e.target.value)} placeholder='Enter wallet address' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full my-8 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'/>
             <button
                 onClick={() => claimToken()}
