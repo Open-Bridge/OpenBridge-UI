05ef59ad513253018f0c3950a689e3229c7dbb77
ahmadou5 <ahmadlasauwal@gmail.com>
2023-12-17 11:10:39 +0300

    'chore:fix-TokenSelect'

--- src/components/Transfer/Details.js
+++ src/components/Transfer/Details.js
@@ -10,7 +10,7 @@ export const MoreDetails = () => {
     <div>
       <div
         className={`bg-gray-50 block  border ${expandDetails &&
-          "h-[190px]"}  border-gray-300 text-gray-900 text-sm outline-none rounded-lg h-12  focus:ring-green-500 focus:border-green-500 w-[98%] p-2.5 dark:bg-green-400/30 dark:border-green-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+          "h-[190px]"} bg-green-400/30 border-gray-300 text-gray-900 text-sm outline-none rounded-lg h-12  focus:ring-green-500 focus:border-green-500 w-[98%] p-2.5 dark:bg-green-400/30 dark:border-green-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
       >
         <div className="flex mb-0 w-[100%]">
           <div className="flex py-1 px-1 ml-3 mr-auto">
--- src/components/Transfer/TokenBridge.js
+++ src/components/Transfer/TokenBridge.js
@@ -6,7 +6,7 @@ const { setIsTokenShowing, selectedToken, selectedTokenLogo ,amount } = GlobalCo
   
   return (
     <div>
-      <div className="bg-gray-50 flex border border-gray-300 text-gray-900 text-sm outline-none rounded-lg h-12  focus:ring-green-500 focus:border-green-500 w-[98%] p-2.5 dark:bg-green-400/30 dark:border-green-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <div className="bg-green-400/30 flex border border-gray-300 text-gray-900 text-sm outline-none rounded-lg h-12  focus:ring-green-500 focus:border-green-500 w-[98%] p-2.5 dark:bg-green-400/30 dark:border-green-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <div  onClick={() => setIsTokenShowing(true)} className="flex py-1 px-1 ml-3 mr-auto">
           <div className=" flex">
             <img className="ml-1 mr-2" src={selectedTokenLogo} />
